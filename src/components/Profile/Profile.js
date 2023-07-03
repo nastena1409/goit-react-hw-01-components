a1d177d15ac4eb8f62586865247de206c6f646d2
nastena1409 <nastena1409@gmail.com>
2023-07-03 13:32:11 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export default function Profile(props) {
     const {
         username,
@@ -35,4 +37,16 @@ export default function Profile(props) {
             </ul>
         </div>
     )
+}
+
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.exact({
+        followers: PropTypes.number,
+        views: PropTypes.number,
+        likes: PropTypes.number
+  }),   
 }
