7d058b6e5f7f38716e36e5b9ea1d8f098a0b9bbd
nastena1409 <nastena1409@gmail.com>
2023-07-03 17:10:19 +0300

    update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,30 +9,30 @@ export default function Profile(props) {
         stats: { followers, views, likes },
     } = props;
     return (
-        <div class="profile">
-            <div class="description">
+        <div className="profile">
+            <div className="description">
                 <img
                     src={avatar}
                     alt="User avatar"
-                    class="avatar"
+                    className="avatar"
                 />
-                <p class="name">{username}</p>
-                <p class="tag">@{tag}</p>
-                <p class="location">{location}</p>
+                <p className="name">{username}</p>
+                <p className="tag">@{tag}</p>
+                <p className="location">{location}</p>
             </div>
 
-            <ul class="stats">
+            <ul className="stats">
                 <li>
-                    <span class="label">Followers</span>
-                    <span class="quantity">{followers}</span>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{followers}</span>
                 </li>
                 <li>
-                    <span class="label">Views</span>
-                    <span class="quantity">{views}</span>
+                    <span className="label">Views</span>
+                    <span className="quantity">{views}</span>
                 </li>
                 <li>
-                    <span class="label">Likes</span>
-                    <span class="quantity">{likes}</span>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{likes}</span>
                 </li>
             </ul>
         </div>
@@ -40,13 +40,13 @@ export default function Profile(props) {
 }
 
 Profile.propTypes = {
-    username: PropTypes.string,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
     stats: PropTypes.exact({
-        followers: PropTypes.number,
-        views: PropTypes.number,
-        likes: PropTypes.number
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired
   }),   
 }
