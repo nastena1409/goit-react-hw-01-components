d291a70071adcd8a3c72812b1aaa48f9a57d5d96
nastena1409 <nastena1409@gmail.com>
2023-07-06 13:43:25 +0300

    Styled Item

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
-
+import {
+    Item,
+    Status,
+    Avatar,
+    Name
+} from '../FriendListItem/FriendListItem.styled'
 
 export default function FriendListItem ({ avatar, name, isOnline }) {
     return (
-        <li className="item">
-            <span className="status" isOnline={isOnline}></span>
-            <img className="avatar" src={avatar} alt={name} width="48" />
-            <p className="name">{name}</p>
-        </li>
+        <Item>
+            <Status isOnline={isOnline}></Status>
+            <Avatar src={avatar} alt={name} width="48" />
+            <Name>{name}</Name>
+        </Item>
     )
 }
--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -1,1 +1,30 @@
+import styled from '@emotion/styled'
 
+export const Item = styled.li`
+display: flex;
+align-items: center;
+gap: 20px;
+border: 2px solid grey;
+border-radius: 15px;
+padding: 15px;
+`;
+
+export const Status = styled.span`
+border-radius: 50%;
+width: 20px;
+height: 20px;
+background-color: ${props => (props.isOnline ? 'green' : 'red')};
+`;
+
+export const Avatar = styled.img`
+padding: 5px;
+width: 80px;
+height: 80px;
+border-radius: 5px;
+border: 2px solid grey;
+`;
+
+export const Name = styled.p`
+font-size: 32px;
+font-weight: 500;
+`;
